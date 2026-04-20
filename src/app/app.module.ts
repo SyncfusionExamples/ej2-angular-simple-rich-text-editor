16a436b645252d4541bd9a9382d937de0fc0b40c
yaswinVikhaash-sf4272 <147583552+yaswinVikhaash-sf4272@users.noreply.github.com>
2026-04-20 12:13:54 +0530

    Need to resolve dependabot issue in Rich Text Editor and Kanban Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {RichTextEditorModule,ToolbarService, LinkService, ImageService, HtmlEditorService} from '@syncfusion/ej2-ng-richtexteditor';
+import { RichTextEditorModule, ToolbarService, LinkService, ImageService, HtmlEditorService } from '@syncfusion/ej2-angular-richtexteditor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule, RichTextEditorModule
+    BrowserModule, RichTextEditorModule, AppComponent
   ],
   providers: [ToolbarService, LinkService, ImageService, HtmlEditorService],
   bootstrap: [AppComponent]
